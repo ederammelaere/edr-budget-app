9b132f22b8a83459f2a4d62a32cfa4178d0776b3
Evert De Rammelaere <e_de_rammelaere@hotmail.com>
2014-04-27 13:53:32 +0200

    interface voor budgetstaat-service

---
+++ edr-budget-app-server/src/main/java/org/edr/services/BudgetStaatService.java
@@ -0,0 +1,9 @@
+package org.edr.services;
+
+import org.edr.domain.BudgetStaat;
+
+public interface BudgetStaatService {
+
+	BudgetStaat getBudgetStaat(int jaar);
+
+}
---
+++ edr-budget-app-server/src/main/java/org/edr/services/impl/StandaardBudgetStaatService.java
@@ -0,0 +1,14 @@
+package org.edr.services.impl;
+
+import org.edr.domain.BudgetStaat;
+import org.edr.services.BudgetStaatService;
+import org.edr.util.services.StandaardAbstractService;
+
+public class StandaardBudgetStaatService extends StandaardAbstractService implements BudgetStaatService {
+
+	@Override
+	public BudgetStaat getBudgetStaat(int jaar) {
+		return null;
+	}
+
+}
