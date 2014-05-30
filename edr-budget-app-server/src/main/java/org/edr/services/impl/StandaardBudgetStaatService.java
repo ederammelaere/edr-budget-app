23c00effca79a26c087768cc9c83ba63e01bffa4
Evert De Rammelaere <e_de_rammelaere@hotmail.com>
2014-05-30 16:18:14 +0200

    refactoring van JPAQL naar JPA CriteriaQuery

--- edr-budget-app-server/src/main/java/org/edr/services/impl/StandaardBudgetStaatService.java
+++ edr-budget-app-server/src/main/java/org/edr/services/impl/StandaardBudgetStaatService.java
@@ -1,32 +1,49 @@
 package org.edr.services.impl;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.persistence.Tuple;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+
 import org.edr.domain.BudgetStaat;
 import org.edr.domain.impl.BudgetStaatDO;
-import org.edr.po.Boeking;
 import org.edr.po.Boekrekening;
 import org.edr.po.Budget;
+import org.edr.po.jpa.BoekingPO;
+import org.edr.po.jpa.BoekingPO_;
+import org.edr.po.jpa.BoekrekeningPO;
+import org.edr.po.jpa.BoekrekeningPO_;
+import org.edr.po.jpa.BudgetPO;
+import org.edr.po.jpa.BudgetPO_;
 import org.edr.services.BudgetStaatService;
 import org.edr.util.domain.RekeningStelselUtil;
 import org.edr.util.services.StandaardAbstractService;
 
 public class StandaardBudgetStaatService extends StandaardAbstractService implements BudgetStaatService {
 
-	@SuppressWarnings("unchecked")
 	@Override
 	public BudgetStaat getBudgetStaat(int jaar) {
 		Map<Boekrekening, BudgetStaat> budgetStaatMap = new HashMap<>();
+
+		CriteriaBuilder cb = entityManager.getCriteriaBuilder();
 
 		//
 		// Vormen van rekeningenstelsel
 		//
 
-		List<Boekrekening> boekRekeningen = (List<Boekrekening>) entityManager.createQuery(
-				"select p from BoekrekeningPO p order by p.rekeningnr").getResultList();
+		CriteriaQuery<Boekrekening> criteriaQueryBoekrekening = cb.createQuery(Boekrekening.class);
+
+		Root<BoekrekeningPO> boekrekeningFrom = criteriaQueryBoekrekening.from(BoekrekeningPO.class);
+		criteriaQueryBoekrekening.select(boekrekeningFrom);
+		criteriaQueryBoekrekening.orderBy(cb.asc(boekrekeningFrom.get(BoekrekeningPO_.rekeningnr)));
+
+		List<Boekrekening> boekRekeningen = entityManager.createQuery(criteriaQueryBoekrekening).getResultList();
 
 		if (boekRekeningen.size() == 0) {
 			return null;
@@ -52,18 +69,35 @@ public class StandaardBudgetStaatService extends StandaardAbstractService implem
 		//
 		// Inlezen van budget
 		//
-		((List<Budget>) entityManager.createQuery("select p from BudgetPO p where jaar = :jaar")
-				.setParameter("jaar", jaar).getResultList()).stream().forEach(s -> {
+
+		CriteriaQuery<Budget> criteriaQueryBudget = cb.createQuery(Budget.class);
+
+		Root<BudgetPO> budgetFrom = criteriaQueryBudget.from(BudgetPO.class);
+		criteriaQueryBudget.select(budgetFrom);
+		criteriaQueryBudget.where(cb.equal(budgetFrom.get(BudgetPO_.jaar), jaar));
+
+		entityManager.createQuery(criteriaQueryBudget).getResultList().forEach(s -> {
 			budgetStaatMap.get(s.getBoekrekening()).addGebudgetteerdBedrag(s.getBedrag());
 		});
 
 		//
 		// Inlezen van boekingen
 		//
-		((List<Boeking>) entityManager.createQuery("select p from BoekingPO p where year(datum) = :jaar")
-				.setParameter("jaar", jaar).getResultList()).stream().forEach(s -> {
-			budgetStaatMap.get(s.getBoekrekening()).addGeboektBedrag(s.getBedrag());
+
+		CriteriaQuery<Tuple> criteriaQueryBoeking = cb.createTupleQuery();
+
+		Root<BoekingPO> boekingFrom = criteriaQueryBoeking.from(BoekingPO.class);
+		criteriaQueryBoeking.select(cb.tuple(boekingFrom.get(BoekingPO_.boekrekening),
+				cb.sum(boekingFrom.get(BoekingPO_.bedrag))));
+		criteriaQueryBoeking
+				.where(cb.equal(cb.function("year", Integer.class, boekingFrom.get(BoekingPO_.datum)), jaar));
+		criteriaQueryBoeking.groupBy(boekingFrom.get(BoekingPO_.boekrekening));
+
+		entityManager.createQuery(criteriaQueryBoeking).getResultList().forEach(s -> {
+			budgetStaatMap.get(s.get(0)).addGeboektBedrag((BigDecimal) s.get(1));
+			;
 		});
+
 		return rootBudgetStaat;
 	}
 
