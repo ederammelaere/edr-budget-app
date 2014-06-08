dc0f2bbbf3b1a54fd5307da8742073323367a47e
Evert De Rammelaere <e_de_rammelaere@hotmail.com>
2014-06-08 11:39:40 +0200

    Tomcat vanuit Eclipse => ${eclipsemap}/conf/application-eclipse.properties

--- edr-budget-app-rest/src/main/java/org/edr/resource/HelloWorldResource.java
+++ edr-budget-app-rest/src/main/java/org/edr/resource/HelloWorldResource.java
@@ -4,13 +4,20 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+
 @Path("helloworld")
 public class HelloWorldResource {
+
+	@Autowired
+	@Value("${jdbc.url}")
+	private String jdbcUrl;
 
 	@GET
 	@Produces("text/plain")
 	public String getHello() {
-		return "Hello world!";
+		return "Hello world! " + jdbcUrl;
 	}
 
 }
