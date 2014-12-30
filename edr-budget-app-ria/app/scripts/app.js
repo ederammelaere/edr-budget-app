76fa5faf01ddc0cf85de016b459475e8ae1c3124
Evert De Rammelaere <e_de_rammelaere@hotmail.com>
2014-12-30 12:12:37 +0100

    probleem met vertrouwen van backend door angular in testopstelling

--- edr-budget-app-ria/app/scripts/app.js
+++ edr-budget-app-ria/app/scripts/app.js
@@ -48,6 +48,15 @@ if (window.location.host=='127.0.0.1:9000') {
 	edrBudgetAppRiaApp.constant('baseRestPath','/edr-budget-app-rest-prod/rest/');
 }
 
+angular.module('edrBudgetAppRiaApp').config(['$sceDelegateProvider', function($sceDelegateProvider) {
+	  $sceDelegateProvider.resourceUrlWhitelist([
+	    // Allow same origin resource loads.
+	    'self',
+	    // Allow loading from our assets domain.  Notice the difference between * and **.
+	    'http://localhost:8080/edr-budget-app-rest/**'
+	  ]);
+	}]);
+
 function resetFormObj($scope)
 {
 	$scope.formObj = {};		
