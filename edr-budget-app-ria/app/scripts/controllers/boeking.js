c45cd3c1d9a1a4f15ec453265096e5a200615ad1
Evert De Rammelaere <e_de_rammelaere@hotmail.com>
2016-06-18 11:31:54 +0200

    opzoeken van referentie boekingen - luik ria module - vervolg

--- edr-budget-app-ria/app/scripts/controllers/boeking.js
+++ edr-budget-app-ria/app/scripts/controllers/boeking.js
@@ -5,39 +5,39 @@ angular.module('edrBudgetAppRiaApp').factory('Boeking', ['$resource', 'baseRestP
 	}]);
 
 angular.module('edrBudgetAppRiaApp')
-  .controller('BoekingCtrl', ['$scope', 'Boeking', 'MyModalWindow', function ($scope, Boeking, MyModalWindow) {
-    
+  .controller('BoekingCtrl', ['$scope', 'Boeking', 'MyModalWindow', '$modal', function ($scope, Boeking, MyModalWindow, $modal) {
+
 	function refresh()
 	{
 		$scope.boekingen = Boeking.query({'jaar' : $scope.jaar});
 	}
-	  
+
 	$scope.jaar = new Date().getFullYear();
 	refresh();
-			
+
 	$scope.$watch("jaar", function(newValue, oldValue){
-		if (newValue === oldValue) return; 
+		if (newValue === oldValue) return;
 		if (newValue > 2000 && newValue < 3000) refresh(); });
-    
+
     $scope.save = function(formObj) {
     	formObj.datum = formObj.datum.split("-").reverse().toString().replace(/,/g, "/");
-    	Boeking.save(addIdObject(formObj), formObj, succesHandler(refresh), errorHandler); 
+    	Boeking.save(addIdObject(formObj), formObj, succesHandler(refresh), errorHandler);
     };
-    
+
     $scope.verwijderen = function(index) {
     	Boeking.remove({ id:$scope.boekingen[index].id }, succesHandler(refresh), errorHandler);
     };
-    
+
     $scope.bijwerken = function(index) {
     	var formObj = angular.copy($scope.boekingen[index]);
     	formObj.datum = formObj.datum.split("/").reverse().toString().replace(/,/g, "-");
     	MyModalWindow.openModal(formObj, $scope.save, 'boekingModal.html');
     };
-    
+
     $scope.toevoegen = function() {
     	MyModalWindow.openModal({}, $scope.save, 'boekingModal.html');
     };
-    
+
     $scope.inputFilter = "";
     $scope.tekstFilter = function(boeking) {
     	if ($scope.inputFilter == "") {
@@ -45,12 +45,46 @@ angular.module('edrBudgetAppRiaApp')
     	}
     	else {
     		var regexpFilter = RegExp($scope.inputFilter, "i");
-    		
-    		return regexpFilter.test(boeking.bankrekening.rekeningnr) || 
-    			regexpFilter.test(boeking.bankrekening.omschrijving) || 
-    			regexpFilter.test(boeking.boekrekening.rekeningnr) || 
-    			regexpFilter.test(boeking.boekrekening.omschrijving) || 
+
+    		return regexpFilter.test(boeking.bankrekening.rekeningnr) ||
+    			regexpFilter.test(boeking.bankrekening.omschrijving) ||
+    			regexpFilter.test(boeking.boekrekening.rekeningnr) ||
+    			regexpFilter.test(boeking.boekrekening.omschrijving) ||
     			regexpFilter.test(boeking.omschrijving);
     	}
     };
+
+    $scope.toevoegenManueel = function() {
+      var modalInstance = $modal.open({
+        templateUrl: 'boekingManueelModal.html',
+        controller: 'BoekingModalInstanceCtrl',
+        windowClass: 'app-modal-window',
+      });
+
+      modalInstance.result.then(function (boekingen) {
+        for(var i = 0; i<boekingen.length; i++) {
+          $scope.save(boekingen[i]);
+        }
+      });
+    };
   }]);
+
+angular.module('edrBudgetAppRiaApp')
+  .controller('BoekingModalInstanceCtrl', ['$scope', '$modalInstance', '$http', 'baseRestPath',
+    function ($scope, $modalInstance, $http, baseRestPath) {
+
+      $http({
+        method: "GET",
+        url: baseRestPath + 'boeking/manueel/2016/1/1'
+      })
+        .success(function (data) {
+          $scope.boekingen = data;
+        });
+
+      var vandaag = new Date();
+      
+      $scope.reset = function () {
+        $modalInstance.dismiss('cancel');
+      };
+
+    }]);
