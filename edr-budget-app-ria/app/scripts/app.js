32c540121d133c185e229d815d73aef57b86bb62
Evert De Rammelaere <e_de_rammelaere@hotmail.com>
2016-06-18 11:31:24 +0200

    opzoeken van referentie boekingen - luik ria module - vervolg

--- edr-budget-app-ria/app/scripts/app.js
+++ edr-budget-app-ria/app/scripts/app.js
@@ -38,99 +38,94 @@ var edrBudgetAppRiaApp = angular.module('edrBudgetAppRiaApp', [
       });
   });
 
-if (window.location.host=='127.0.0.1:9000') {
-	edrBudgetAppRiaApp.constant('baseRestPath','http://localhost:8080/edr-budget-app-rest/rest/');
+if (window.location.host == '127.0.0.1:9000') {
+  edrBudgetAppRiaApp.constant('baseRestPath', 'http://localhost:8080/edr-budget-app-rest/rest/');
 } else {
-	edrBudgetAppRiaApp.constant('baseRestPath','/edr-budget-app-rest-prod/rest/');
+  edrBudgetAppRiaApp.constant('baseRestPath', '/edr-budget-app-rest-prod/rest/');
 }
 
-angular.module('edrBudgetAppRiaApp').config(['$sceDelegateProvider', function($sceDelegateProvider) {
-	  $sceDelegateProvider.resourceUrlWhitelist([
-	    // Allow same origin resource loads.
-	    'self',
-	    // Allow loading from our assets domain.  Notice the difference between * and **.
-	    'http://localhost:8080/edr-budget-app-rest/**'
-	  ]);
-	}]);
+angular.module('edrBudgetAppRiaApp').config(['$sceDelegateProvider', function ($sceDelegateProvider) {
+  $sceDelegateProvider.resourceUrlWhitelist([
+    // Allow same origin resource loads.
+    'self',
+    // Allow loading from our assets domain.  Notice the difference between * and **.
+    'http://localhost:8080/edr-budget-app-rest/**'
+  ]);
+}]);
 
-function resetFormObj($scope)
-{
-	$scope.formObj = {};		
+function resetFormObj($scope) {
+  $scope.formObj = {};
 }
 
-function addFalse(formObj, property)
-{
-	if (!formObj[property])
-	{
-		formObj[property] = false;
-	}
+function addFalse(formObj, property) {
+  if (!formObj[property]) {
+    formObj[property] = false;
+  }
 }
 
-function addId($scope)
-{
-	if ($scope.formObj.id)
-		return {'id': $scope.formObj.id};
-	else
-		return {};
+function addId($scope) {
+  if ($scope.formObj.id)
+    return {'id': $scope.formObj.id};
+  else
+    return {};
 }
 
-function addIdObject(formObj)
-{
-	if (formObj.id)
-		return {'id': formObj.id};
-	else
-		return {};
+function addIdObject(formObj) {
+  if (formObj.id)
+    return {'id': formObj.id};
+  else
+    return {};
 }
 
-function errorHandler(error)
-{
-	alert("Fout gebeurd...");
+function errorHandler(error) {
+  alert("Fout gebeurd...");
 }
 
-function succesHandler(functie)
-{
-	return function(){functie();};
+function succesHandler(functie) {
+  return function () {
+    functie();
+  };
 }
 
-angular.module('edrBudgetAppRiaApp').factory('MyModalWindow', ['$modal', function($modal){
-	return {
-		openModal : function (formObj, save, templateUrl) {
+angular.module('edrBudgetAppRiaApp').factory('MyModalWindow', ['$modal', function ($modal) {
+  return {
+    openModal: function (formObj, save, templateUrl) {
 
-			var modalInstance = $modal.open({
-				templateUrl: templateUrl,
-				controller: 'ModalInstanceCtrl',
-				resolve: {
-					formObj: function () {
-						return formObj;
-					}
-				}
-			});
-	    
-			modalInstance.result.then(function (formObj) {
-				save(formObj);
-			});
-		}
-	};
+      var modalInstance = $modal.open({
+        templateUrl: templateUrl,
+        controller: 'ModalInstanceCtrl',
+        resolve: {
+          formObj: function () {
+            return formObj;
+          }
+        }
+      });
+
+      modalInstance.result.then(function (formObj) {
+        save(formObj);
+      });
+    }
+  };
 }]);
 
 angular.module('edrBudgetAppRiaApp')
-	.controller('ModalInstanceCtrl', ['$scope', '$modalInstance', 'formObj', 'Bankrekening', 'Boekrekening', 
-	                                  function ($scope, $modalInstance, formObj, Bankrekening, Boekrekening) {
-	$scope.formObj = formObj;
-	$scope.actie = (formObj.id) ? 'Bewerken' : 'Toevoegen';
-	
-	$scope.initBankrekeningen = function(){
-		$scope.bankrekeningen = Bankrekening.query();		
-	};
-	
-	$scope.initBoekrekeningen = function(){
-		$scope.boekrekeningen = Boekrekening.query();		
-	};
-		
-	$scope.reset = function(){
-    	$modalInstance.dismiss('cancel');
-    };
-    $scope.save = function(){
-    	$modalInstance.close($scope.formObj);
-    };	    
-}]);
+  .controller('ModalInstanceCtrl', ['$scope', '$modalInstance', 'formObj', 'Bankrekening', 'Boekrekening',
+    function ($scope, $modalInstance, formObj, Bankrekening, Boekrekening) {
+      $scope.formObj = formObj;
+      $scope.actie = (formObj.id) ? 'Bewerken' : 'Toevoegen';
+
+      $scope.initBankrekeningen = function () {
+        $scope.bankrekeningen = Bankrekening.query();
+      };
+
+      $scope.initBoekrekeningen = function () {
+        $scope.boekrekeningen = Boekrekening.query();
+      };
+
+      $scope.reset = function () {
+        $modalInstance.dismiss('cancel');
+      };
+      $scope.save = function () {
+        $modalInstance.close($scope.formObj);
+      };
+    }]);
